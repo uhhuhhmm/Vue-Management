8453926bbef6dc3eda4675b981c7421ee4720927
Jin <uwtf0mg@gmail.com>
2022-07-06 17:53:32 +0900

    master

---
+++ webpack/src/components/TodoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
