751764113e1499f38cb0bca038b2aabb6cff7873
Jin <uwtf0mg@gmail.com>
2022-07-06 15:55:36 +0900

    master

---
+++ q/js/q2_app.js
@@ -0,0 +1,22 @@
+// sibling-component를 이름으로 갖는 새로운 컴포넌트를 아래에 등록해보세요.
+// options: template, props
+
+Vue.component("sibling-component", {
+  props: ["propsdata"],
+  template: "<p>{{propsdata}}</p>",
+});
+
+Vue.component("child-component", {
+  props: ["propsdata"],
+  template: "<p>{{propsdata}}</p>",
+});
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!! I kill you as soon as possible",
+    anothermessage: "Hello?",
+    // data 속성을 1개 더 지정하고 (예: anotherMessage) 임의의 문자열을 값으로 대입해보세요.
+    // 새로 지정한 data 속성은 위 sibling-component에 props로 전달합니다.
+  },
+});
